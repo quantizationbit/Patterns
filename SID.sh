bdf6e59a77aa04cf61aae5541386bb04c58638ea
Bill Mandel <quantizationbit@gmail.com>
2016-12-24 22:10:49 -0800

    patterns

--- SID.sh
+++ SID.sh
@@ -19,41 +19,52 @@ set -x
 make
 
 function YUVBuild {
-
+mv ANSI*.tiff ANSI.tiff
+ctlrender -force -ctl $EDRHOME/ACES/CTLa1/Full-2-VideoRange.ctl ANSI.tiff x.tiff
 for i in $(seq 1 120);
 do
-$EDRHOME/Tools/YUV/tif2yuv ANSI*.tiff B10 2020  -o SID.yuv
+$EDRHOME/Tools/YUV/tif2yuv x.tiff B10 2020  -o SID.yuv
 done
-rm -fv ANSI*.tiff
+rm -fv *.tiff
 }
 
 function ENCODE {
 YUVBASE="SID"
 
 # Encode
+FFMPEG="/home/qbit/Documents/EDR/src/ffmpeg/ffmpeg-git-20161217-64bit-static/ffmpeg-10bit"
 crf=12
 
 (x265 --input SID.yuv \
    --input-depth 10 --input-res 3840x2160 --fps 23.976 \
    --profile main10 --level-idc 51 --no-high-tier \
-   --crf $crf --aq-mode 3  \
-    -p slow  --bframes 12 -I 72 --sar 1 --range limited \
+   --crf $crf --aq-mode 0  \
+   --bframes 12 -I 72 --sar 1 --range limited \
    --colorprim bt2020 --transfer 16 --colormatrix bt2020nc --chromaloc 2 \
    --master-display "G(13250,34500)B(7500,3000)R(34000,16000)WP(15635,16450)L(10000000,50)" \
    --max-cll "999,399" \
    --repeat-headers  \
    -o x265-$YUVBASE-$crf.bin ) 2>&1 | tee logencoder-x265-$YUVBASE-$crf.txt
 
-ffmpeg -y -f lavfi -i aevalsrc=0:0:0:0:0:0::d=300  \
+$FFMPEG -y -f lavfi -i aevalsrc=0:0:0:0:0:0::d=300  \
  -ar 48000 \
- -vn -ac 6 -acodec libfdk_aac -cutoff 18000 -ab 768k \
+ -vn -ac 6 -acodec aac -cutoff 18000 -ab 768k \
   audio.aac
-
-MP4Box -v -add x265-$YUVBASE-$crf".bin#0:FMT=HEVC:fps=23.98"   -add "audio.aac:lang=en" -hint -rap  -new x265-$YUVBASE-$crf"-24Hz-cloc2.mp4"
+  
+  
+$FFMPEG -y \
+  -i x265-$YUVBASE-$crf".bin" \
+  -i audio.aac \
+  -c copy x265-$YUVBASE-$crf"-24Hz-cloc2.mp4"
+  
+#MP4Box -add x265-$YUVBASE-$crf".bin:FMT=HEVC"   -add "audio.aac:lang=en"  -new x265-$YUVBASE-$crf"-24Hz-cloc2.mp4"
 }	
 
 
 #main
+#cd SID
+#ENCODE
+#exit
 
 
 rm -rfv SID
@@ -61,55 +72,55 @@ mkdir SID
 cd SID
 rm -fv SID.yuv
 
-../pattern8  -min 0.0 -max 0.0  -2020; YUVBuild
-convert ANSI*.tiff -gravity Center  -pointsize 300 -fill 'rgb(25,0,0)' -annotate 0 'ANSI 0,0\n(no corner)' ANSI.tiff
+../pattern8  -min 0.0 -max 0.0  -2020; mv ANSI*.tiff x.tiff
+convert x.tiff -gravity Center  -pointsize 200 -fill 'rgb(35,0,0)' -annotate 0 'ANSI 0,[0.01, 0.02, 0.05, 0.1]\n(no corner)' ANSI.tiff; rm -fv x.tiff;ctlrender -force -ctl $EDRHOME/ACES/CTL/null.ctl ANSI.tiff -format tiff16 x.tiff;mv -fv x.tiff ANSI.tiff;YUVBuild
 ../pattern8  -min 0.000 -max 0.01 -2020; YUVBuild
 ../pattern8  -min 0.000 -max 0.02 -2020; YUVBuild
 ../pattern8  -min 0.000 -max 0.05 -2020; YUVBuild
 ../pattern8  -min 0.000 -max 0.1  -2020; YUVBuild
 
-../pattern8  -min 0.0 -max 0.0  -2020; YUVBuild
-convert ANSI*.tiff -gravity Center  -pointsize 300 -fill 'rgb(25,0,0)' -annotate 0 'ANSI 0.005,[0.01, 0.02, 0.05, 0.1]\n(no corner)' ANSI.tiff
+../pattern8  -min 0.0 -max 0.0  -2020; mv ANSI*.tiff x.tiff
+convert x.tiff -gravity Center  -pointsize 200 -fill 'rgb(35,0,0)' -annotate 0 'ANSI 0.005,[0.01, 0.02, 0.05, 0.1]\n(no corner)' ANSI.tiff; rm -fv x.tiff;ctlrender -force -ctl $EDRHOME/ACES/CTL/null.ctl ANSI.tiff -format tiff16 x.tiff;mv -fv x.tiff ANSI.tiff;YUVBuild
 ../pattern8  -min 0.005 -max 0.01 -2020; YUVBuild
 ../pattern8  -min 0.005 -max 0.02 -2020; YUVBuild
 ../pattern8  -min 0.005 -max 0.05 -2020; YUVBuild
 ../pattern8  -min 0.005 -max 0.1  -2020; YUVBuild
 
-../pattern8  -min 0.0 -max 0.0  -2020; YUVBuild
-convert ANSI*.tiff -gravity Center  -pointsize 300 -fill 'rgb(25,0,0)' -annotate 0 'ANSI 0.005,[0.01, 0.02, 0.05, 0.1]\n2.5% Corners @ 1.0 cd/m2' ANSI.tiff
+../pattern8  -min 0.0 -max 0.0  -2020; mv ANSI*.tiff x.tiff
+convert x.tiff -gravity Center  -pointsize 200 -fill 'rgb(35,0,0)' -annotate 0 'ANSI 0.005,[0.01, 0.02, 0.05, 0.1]\n2.5% Corners @ 1.0 cd/m2' ANSI.tiff; rm -fv x.tiff;ctlrender -force -ctl $EDRHOME/ACES/CTL/null.ctl ANSI.tiff -format tiff16 x.tiff;mv -fv x.tiff ANSI.tiff;YUVBuild
 ../pattern8  -min 0.005 -max 0.01 -maxC  1.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.005 -max 0.02 -maxC  1.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.005 -max 0.05 -maxC  1.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.005 -max 0.1  -maxC  1.0 -corner -2020; YUVBuild
 
-../pattern8  -min 0.0 -max 0.0  -2020; YUVBuild
-convert ANSI*.tiff -gravity Center  -pointsize 300 -fill 'rgb(25,0,0)' -annotate 0 'ANSI 0.005,[0.01, 0.02, 0.05, 0.1]\n2.5% Corners @ 5.0 cd/m2' ANSI.tiff
+../pattern8  -min 0.0 -max 0.0  -2020; mv ANSI*.tiff x.tiff
+convert x.tiff -gravity Center  -pointsize 200 -fill 'rgb(35,0,0)' -annotate 0 'ANSI 0.005,[0.01, 0.02, 0.05, 0.1]\n2.5% Corners @ 5.0 cd/m2' ANSI.tiff; rm -fv x.tiff;ctlrender -force -ctl $EDRHOME/ACES/CTL/null.ctl ANSI.tiff -format tiff16 x.tiff;mv -fv x.tiff ANSI.tiff;YUVBuild
 ../pattern8  -min 0.005 -max 0.01 -maxC  5.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.005 -max 0.02 -maxC  5.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.005 -max 0.05 -maxC  5.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.005 -max 0.1  -maxC  5.0 -corner -2020; YUVBuild
 
-../pattern8  -min 0.0 -max 0.0  -2020; YUVBuild
-convert ANSI*.tiff -gravity Center  -pointsize 300 -fill 'rgb(25,0,0)' -annotate 0 'ANSI (0.05,0.5), (0.1,0.5), (0.1,1.0)\n2.5% Corners @ 5.0 cd/m2' ANSI.tiff
+../pattern8  -min 0.0 -max 0.0  -2020; mv ANSI*.tiff x.tiff
+convert x.tiff -gravity Center  -pointsize 200 -fill 'rgb(35,0,0)' -annotate 0 'ANSI (0.05,0.5), (0.1,0.5), (0.1,1.0)\n2.5% Corners @ 5.0 cd/m2' ANSI.tiff; rm -fv x.tiff;ctlrender -force -ctl $EDRHOME/ACES/CTL/null.ctl ANSI.tiff -format tiff16 x.tiff;mv -fv x.tiff ANSI.tiff;YUVBuild
 ../pattern8  -min 0.05 -max 0.5 -maxC  5.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.1  -max 0.5 -maxC  5.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.1  -max 1.0 -maxC  5.0 -corner -2020; YUVBuild
 
 
-../pattern8  -min 0.0 -max 0.0  -2020; YUVBuild
-convert ANSI*.tiff -gravity Center  -pointsize 300 -fill 'rgb(25,0,0)' -annotate 0 'ANSI (0.05,0.5), (0.1,0.5), (0.1,1.0)\n2.5% Corners @ 25.0 cd/m2' ANSI.tiff
+../pattern8  -min 0.0 -max 0.0  -2020; mv ANSI*.tiff x.tiff
+convert x.tiff -gravity Center  -pointsize 200 -fill 'rgb(35,0,0)' -annotate 0 'ANSI (0.05,0.5), (0.1,0.5), (0.1,1.0)\n2.5% Corners @ 25.0 cd/m2' ANSI.tiff; rm -fv x.tiff;ctlrender -force -ctl $EDRHOME/ACES/CTL/null.ctl ANSI.tiff -format tiff16 x.tiff;mv -fv x.tiff ANSI.tiff;YUVBuild
 ../pattern8  -min 0.05 -max 0.5 -maxC  25.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.1  -max 0.5 -maxC  25.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.1  -max 1.0 -maxC  25.0 -corner -2020; YUVBuild
 
-../pattern8  -min 0.0 -max 0.0  -2020; YUVBuild
-convert ANSI*.tiff -gravity Center  -pointsize 300 -fill 'rgb(25,0,0)' -annotate 0 'ANSI (0.05,0.5), (0.1,0.5), (0.1,1.0)\n2.5% Corners @ 250.0 cd/m2' ANSI.tiff
+../pattern8  -min 0.0 -max 0.0  -2020; mv ANSI*.tiff x.tiff
+convert x.tiff -gravity Center  -pointsize 200 -fill 'rgb(35,0,0)' -annotate 0 'ANSI (0.05,0.5), (0.1,0.5), (0.1,1.0)\n2.5% Corners @ 250.0 cd/m2' ANSI.tiff; rm -fv x.tiff;ctlrender -force -ctl $EDRHOME/ACES/CTL/null.ctl ANSI.tiff -format tiff16 x.tiff;mv -fv x.tiff ANSI.tiff;YUVBuild
 ../pattern8  -min 0.05 -max 0.5 -maxC  250.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.1  -max 0.5 -maxC  250.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.1  -max 1.0 -maxC  250.0 -corner -2020; YUVBuild
 
-../pattern8  -min 0.0 -max 0.0  -2020; YUVBuild
-convert ANSI*.tiff -gravity Center  -pointsize 300 -fill 'rgb(25,0,0)' -annotate 0 'ANSI (0.05,0.5), (0.1,0.5), (0.1,1.0)\n2.5% Corners @ 400.0 cd/m2' ANSI.tiff
+../pattern8  -min 0.0 -max 0.0  -2020; mv ANSI*.tiff x.tiff
+convert x.tiff -gravity Center  -pointsize 200 -fill 'rgb(35,0,0)' -annotate 0 'ANSI (0.05,0.5), (0.1,0.5), (0.1,1.0)\n2.5% Corners @ 400.0 cd/m2' ANSI.tiff; rm -fv x.tiff;ctlrender -force -ctl $EDRHOME/ACES/CTL/null.ctl ANSI.tiff -format tiff16 x.tiff;mv -fv x.tiff ANSI.tiff;YUVBuild
 ../pattern8  -min 0.05 -max 0.5 -maxC  400.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.1  -max 0.5 -maxC  400.0 -corner -2020; YUVBuild
 ../pattern8  -min 0.1  -max 1.0 -maxC  400.0 -corner -2020; YUVBuild
